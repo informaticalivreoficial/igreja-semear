ecb4598e731ae8aeb204e02bacc68fd285c895fe
renato-Aspire-E5-574 <renatomontanari@ymail.com>
2023-11-24 20:50:51 -0300

    front update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -46,9 +46,6 @@ class AppServiceProvider extends ServiceProvider
         $configuracoes = \App\Models\Configuracoes::find(1); 
         View()->share('configuracoes', $configuracoes);
 
-
-        
-        
         Paginator::useBootstrap();
     }
 }
